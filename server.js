a3b3d75ba8fc85284551e271d82101533f965b8f
Alevs97 <alejandro.viveros.s@outlook.es>
2020-12-29 22:12:06 -0600

    Primer commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+
+const hbs = require('hbs');
+
+const port = process.env.PORT || 3000;
+
+require(__dirname + '/hbs/helpers');
+
+//midelware
+app.use(express.static(__dirname + '/public'));
+
+//Usar como parciales 
+hbs.registerPartials(__dirname + '/views/parciales');
+
+//Uso de hbs para renderizar
+app.set('view engine', 'hbs');
+
+//Podemos escoger pormedio del primer parametro express
+app.get('/', (req, res) => {
+    //Denderizar los .hbs
+    res.render('home', {
+        nombre: 'Alejandro',
+        anio: new Date().getFullYear()
+    });
+});
+
+app.get('/about', (req, res) => {
+    //Denderizar los .hbs
+    res.render('about', {
+        anio: new Date().getFullYear()
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Esta escuchando en el puesto ${port}`);
+})
